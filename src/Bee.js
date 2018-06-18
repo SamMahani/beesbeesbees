436d39449d7b459136d73202b447edac7bc7aaf8
samMahani <sam.amirmahani@gmail.com>
2018-06-18 16:33:55 -0700

    passed all tests for BeesBeesBees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  //call method gives ability to specify what "this" gets bound to
+  Grub.call(this);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing"
 };
 
+//set the prototype so we have a delegation to the factory class' properties 
+  Bee.prototype = Object.create(Grub.prototype);
+
+//set the constructor
+  Bee.prototype.constructor = Bee;
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,10 @@
 var Grub = function() {
+// object creation using this
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
+// ret of this
 };
 
+Grub.prototype.eat = function() {
+};
